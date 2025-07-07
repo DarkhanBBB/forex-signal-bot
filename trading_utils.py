d83025e74a7b3eca7a0fad1623050616f003b61f
DarkhanBBB <bdarkhanb@gmail.com>
2025-07-07 09:55:54 +0500

    Удалил импорт smart_money_indicators

--- trading_utils.py
+++ trading_utils.py
@@ -1,13 +1,5 @@
 import numpy as np
 import pandas as pd
-from smart_money_indicators import detect_bos, detect_fvg, detect_order_blocks, detect_liquidity_zones
-from smart_money_indicators import (
-    detect_bos,
-    detect_fvg,
-    detect_order_blocks,
-    detect_liquidity_zones,
-    add_smart_money_indicators,
-)
 
 def prepare_data(df):
     df = df.copy()
